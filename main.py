35ab2bc64ece5fd567a451f4981dfcdc2079d047
jmendez44 <jenuel.mendez@gmail.com>
2020-10-15 18:10:32 -0400

    bot working

---
+++ main.py
@@ -0,0 +1,92 @@
+import socket
+from collections import namedtuple
+# import config
+
+Message = namedtuple(
+    'Message',
+    'prefix user channel irc_command irc_args text text_command text_args',
+)
+
+
+class Bot:
+    def __init__(self):
+        self.irc_server = 'irc.twitch.tv'
+        self.irc_port = 6667
+        self.oauth_token = 'oauth:lqskz133wvv08w070tnlqz0pxqq1cp'
+        self.username = 'l0tso'
+        self.channels = ['l0tso']
+
+    def send_privmsg(self, channel, text):
+        self.send_command(f'PRIVMSG #{channel} : {text}')
+
+    def send_command(self, command):
+        if 'PASS' not in command:
+            print(f'< {command}')
+        self.irc.send((command + '\r\n').encode())
+
+    def connect(self):
+        self.irc = socket.socket()
+        self.irc.connect((self.irc_server, self.irc_port))
+        self.send_command(f'PASS {self.oauth_token}')
+        self.send_command(f'NICK {self.username}')
+        for channel in self.channels:
+            self.send_command(f'JOIN #{channel}')
+            self.send_privmsg(channel, 'hey there')
+        self.loop_for_messages()
+
+    def get_user_from_prefix(self, prefix):
+        domain = prefix.split('!')[0]
+        if domain.endswith('.tmi.twitch.tv'):
+            return domain.replace('.tmi.twitch.tv', '')
+        if 'tmi.twitch.tv' not in domain:
+            return domain
+        return None
+
+    def parse_message(self, received_msg):
+        parts = received_msg.split(' ')
+        
+        prefix = None
+        user = None
+        channel = None
+        text = None
+        text_command = None
+        text_args = None
+        irc_command = None
+        irc_args = None
+
+        if parts[0].startswith(':'):
+            prefix = parts[0][1:]
+            user = self.get_user_from_prefix(prefix)
+            parts = parts[1:]
+
+        message = Message(
+            prefix=prefix,
+            user=user,
+            channel=channel,
+            text=text,
+            text_command=text_command,
+            text_args=text_args,
+            irc_command=irc_command,
+            irc_args=irc_args,
+        )
+
+        return message
+
+    def handle_message(self, received_msg):
+        message = self.parse_message(received_msg)
+        print(f'> {message}')
+
+    def loop_for_messages(self):
+        while True:
+            received_msgs = self.irc.recv(2048).decode()
+            for received_msg in received_msgs.split('\r\n'):
+                self.handle_message(received_msg)
+
+
+def main():
+    bot = Bot()
+    bot.connect()
+
+
+if __name__ == '__main__':
+    main()
